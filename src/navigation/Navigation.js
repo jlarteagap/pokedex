80a93ed9b2c3629282d0fed1459df07e3505ece0
Jorge <jorgelarteagap@gmail.com>
2022-02-27 20:26:48 -0400

    [FEAT] react navite .64.3

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -1,15 +1,33 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
+import Icon from "react-native-vector-icons/FontAwesome";
 import { Account, Pokedex, Favorite } from "../screens/";
 const Tab = createBottomTabNavigator();
 
 export default function Navigation() {
   return (
     <Tab.Navigator>
-      <Tab.Screen component={Pokedex} name="Pokedex" />
+      <Tab.Screen
+        component={Pokedex}
+        name="Pokedex"
+        options={{
+          tabBarLabel: "Favoritos",
+          tabBarIcon: ({ color, size }) => {
+            <Icon name="heart" color={color} size={size} />;
+          },
+        }}
+      />
       <Tab.Screen component={Favorite} name="Favorite" />
-      <Tab.Screen component={Account} name="Account" />
+      <Tab.Screen
+        component={Account}
+        name="Account"
+        options={{
+          tabBarLabel: "Mi cuenta",
+          tabBarIcon: ({ color, size }) => {
+            <Icon name="user" color={color} size={size} />;
+          },
+        }}
+      />
     </Tab.Navigator>
   );
 }
